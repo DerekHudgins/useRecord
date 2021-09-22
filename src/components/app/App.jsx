cd1f8027958a7fe9e62b6981d0ff8ee0610e1bb1
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-22 16:01:07 -0700

    brought in starter code

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+const useRecord = (init) => {};
+
+function App() {
+  const { current, undo, redo, record } = useRecord('#FF0000');
+
+  return (
+    <>
+      <button onClick={undo}>undo</button>
+      <button onClick={redo}>redo</button>
+      <input
+        type="color"
+        value={current}
+        onChange={({ target }) => record(target.value)}
+      />
+      <div
+        style={{ backgroundColor: current, width: '10rem', height: '10rem' }}
+      ></div>
+    </>
+  );
 }
+
+export default App;
